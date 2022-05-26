a720ffe69e30842086aa00fbee553745f6a1ae3b
JhandrySolorzano <jhandri_07@hotmail.com>
2022-05-26 14:09:46 -0500

    Cambios

---
+++ paquete02/Doctor.java
@@ -0,0 +1,38 @@
+package paquete02;
+
+
+public class Doctor {
+    private String nombre;
+    private String especialidad;
+    private double sueldo;
+    
+    public Doctor(String nom, String ep, double s) {
+        nombre = nom;
+        especialidad = ep;
+        sueldo = s;
+    }
+    
+    public void establecerNombre(String x) {
+        nombre = x;
+    }
+    
+    public void establecerEspecialidad(String x) {
+        especialidad = x;
+    }
+    
+    public void establecerSueldo(double x) {
+        sueldo = x;
+    }
+    
+    public String obtenerNombre() {
+        return nombre;
+    }
+    
+    public String obtenerEspecialidad() {
+        return especialidad;
+    }
+    
+    public double obtenerSueldo() {
+        return sueldo;
+    }
+}
